a0805c9960073968b53645cb226e7f62fc7b77a4
c5sire <rsimon64@gmail.com>
2015-09-30 10:37:16 -0500

    added crops

--- R/fname_sites.R
+++ R/fname_sites.R
@@ -7,11 +7,21 @@ fname_sites <- function() {
   file.path("xdata", "table_sites.rda")
 }
 
+#' Fieldbook crops path
+#'
+#' @author Reinhard Simon
+#' @return character file.path
+#' @export
+fname_crops <- function(){
+  file.path("xdata", "table_crops.rda")
+}
+
+
 #' Ontology path
 #'
 #' @author Reinhard Simon
 #' @return character file.path
 #' @export
 fname_ontology <- function() {
-  file.path("data", "dictionary")
+  file.path("xdata", "dictionary")
 }
